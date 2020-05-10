3185af5267af4760bd22a6d7f72df8909ccec9d3
acrio <wry0618@gmail.com>
2020-05-10 14:52:15 -0700

    Create #159-longest-substring-with-at-most-two-distinct-characters.java

---
+++ #159-longest-substring-with-at-most-two-distinct-characters.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int lengthOfLongestSubstringTwoDistinct(String s) {
+        Map<Character,Integer> map = new HashMap<>();
+        int left = 0, right = 0;
+        int counter = 0;
+        int maxLen = 0;
+
+        while (right < s.length()) {
+            char cRight = s.charAt(right);
+            map.put(cRight, map.getOrDefault(cEnd, 0) + 1); 
+
+            while (map.size() > 2) {
+                char cLeft = s.charAt(left);
+                map.put(cLeft, map.get(cLeft) - 1);
+                map.remove(cLeft, 0);
+                left++;
+            }
+            
+            maxLen = Math.max(maxLen, end - begin);
+            right++;
+        }
+
+        return maxLen;
+    }
+}
